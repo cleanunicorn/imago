4d4595723fd8fb8d93dfa7d054f766a24e798925
Daniel Luca <lucadanielcostin@gmail.com>
2020-02-23 19:14:07 +0200

    [WIP] Truffle setup.

---
+++ truffle.js
@@ -0,0 +1,39 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+
+  networks: {
+    ganache: {
+      hardfork: "istanbul",
+      network_id: 127001,
+      host: "127.0.0.1",
+      port: 8545
+    }
+  },
+
+  compilers: {
+    solc: {
+      version: "./node_modules/solc"
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 100000,
+      details: {
+        yul: true,
+        constantOptimizer: false
+      }
+    }
+  },
+
+  mocha: {
+    reporter: "eth-gas-reporter",
+    reporterOptions: {
+      currency: "USD",
+      gasPrice: 10,
+      showTimeSpent: true
+    }
+  }
+
+}

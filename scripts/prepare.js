8b8a880742de365e07f3e2c4e36c8a27fef5f543
Daniel Luca <lucadanielcostin@gmail.com>
2020-03-10 13:44:24 +0100

    Lint prepare.js.

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -2,44 +2,44 @@
 
 // This script removes the build artifacts of ignored contracts.
 
-const fs = require('fs');
-const path = require('path');
-const cp = require('child_process');
-const match = require('micromatch');
+const fs = require('fs')
+const path = require('path')
+const cp = require('child_process')
+const match = require('micromatch')
 
 function readJSON (path) {
-  return JSON.parse(fs.readFileSync(path));
+    return JSON.parse(fs.readFileSync(path))
 }
 
-cp.spawnSync('npm', ['run', 'compile'], { stdio: 'inherit' });
+cp.spawnSync('npm', ['run', 'compile'], { stdio: 'inherit' })
 
-const pkgFiles = readJSON('package.json').files;
+const pkgFiles = readJSON('package.json').files
 
 // Get only negated patterns.
 const ignorePatterns = pkgFiles
-  .filter(pat => pat.startsWith('!'))
-// Remove the negation part. Makes micromatch usage more intuitive.
-  .map(pat => pat.slice(1));
+    .filter(pat => pat.startsWith('!'))
+    // Remove the negation part. Makes micromatch usage more intuitive.
+    .map(pat => pat.slice(1))
 
 const ignorePatternsSubtrees = ignorePatterns
-// Add **/* to ignore all files contained in the directories.
-  .concat(ignorePatterns.map(pat => path.join(pat, '**/*')));
+    // Add **/* to ignore all files contained in the directories.
+    .concat(ignorePatterns.map(pat => path.join(pat, '**/*')))
 
-const artifactsDir = 'build/contracts';
+const artifactsDir = 'build/contracts'
 
-let n = 0;
+let n = 0
 
 for (const artifact of fs.readdirSync(artifactsDir)) {
-  const fullArtifactPath = path.join(artifactsDir, artifact);
-  const { sourcePath: fullSourcePath } = readJSON(fullArtifactPath);
-  const sourcePath = path.relative('.', fullSourcePath);
+    const fullArtifactPath = path.join(artifactsDir, artifact)
+    const { sourcePath: fullSourcePath } = readJSON(fullArtifactPath)
+    const sourcePath = path.relative('.', fullSourcePath)
 
-  const ignore = match.any(sourcePath, ignorePatternsSubtrees);
+    const ignore = match.any(sourcePath, ignorePatternsSubtrees)
 
-  if (ignore) {
-    fs.unlinkSync(fullArtifactPath);
-    n += 1;
-  }
+    if (ignore) {
+        fs.unlinkSync(fullArtifactPath)
+        n += 1
+    }
 }
 
-console.error(`Removed ${n} mock artifacts`);
+console.error(`Removed ${n} mock artifacts`)

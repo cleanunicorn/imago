7d707d3cbb5c4908c99c4fc97ffcee04e03bb977
Daniel Luca <lucadanielcostin@gmail.com>
2020-03-10 14:44:50 +0100

    [WIP] Implement ERC20 behavior.

--- test/Imago.test.js
+++ test/Imago.test.js
@@ -1,16 +1,26 @@
-const { contract } = require('@openzeppelin/test-environment')
+const { accounts, contract } = require('@openzeppelin/test-environment')
 const { expect } = require('chai')
 
-require('@openzeppelin/test-helpers')
+const {
+    shouldBehaveLikeERC20,
+} = require('./ERC20/ERC20.behavior');
+
+const { BN } = require('@openzeppelin/test-helpers')
 
 const Imago = contract.fromArtifact('Imago')
 
+const initialSupply = new BN('10000');
 const name = 'Imago'
 const symbol = 'IMG'
 
 describe('Imago', () => {
     beforeEach(async () => {
         this.imago = await Imago.new(name, symbol)
+    })
+
+    context('ERC20', () => {
+        const [ holder, defaultOperatorA, anyone ] = accounts;
+        shouldBehaveLikeERC20('ERC777', initialSupply, holder, anyone, defaultOperatorA);
     })
 
     context('deploy', () => {
